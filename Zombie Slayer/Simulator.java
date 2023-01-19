f88388300a882d4d12f5bff92bbed2f48c6e97b2
Melike Kurt <melikekurt315@gmail.com>
2023-01-19 18:59:22 +0300

    Add files via upload

---
+++ Zombie Slayer/Simulator.java
@@ -0,0 +1,7 @@
+import jdk.javadoc.internal.doclets.formats.html.resources.standard;
+
+public class Simulator {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Zombie Slayer/Slayer.java
@@ -0,0 +1,18 @@
+public class Slayer extends Thread {
+    ZombieCounter zc;
+    public Slayer(ZombieCounter zc){
+        this.zc = zc;
+    }
+    @Override
+    public void run(){
+        while(!zc.tooManyZombiesInTheRoom() && !zc.killed100Zombies()){
+            zc.zombieKilled();
+            try{
+            Thread.sleep(2000);
+            }
+        catch(InterruptedException e){
+                e.printStackTrace();
+            }
+       }    
+    }
+}
